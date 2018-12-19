7a6d17d657f26c3ee9e00bf4c105b9755f59ca32
azeem <azim5289007@gmail.com>
2018-12-19 13:12:27 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ var app = express();
 
 app.use(bodyParser.json());
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.post('/user', function (req, res) {
     let id = req.body.id;
     let token = req.body.token;
